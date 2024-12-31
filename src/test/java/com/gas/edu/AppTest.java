340e0239647af5ac02587149e373a46527d26b4c
N2-SGASPERI\s.gasperi <stefano.gasperi@gmail.com>
2024-12-31 10:59:41 +0100

    log4j configuration

--- src/test/java/com/gas/edu/AppTest.java
+++ src/test/java/com/gas/edu/AppTest.java
@@ -2,7 +2,6 @@ package com.gas.edu;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AppTest {
